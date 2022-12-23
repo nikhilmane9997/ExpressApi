61511cee274c2b6303fd09ab23fe368e4b9815e7
nikhilmane9997 <nikhilmane56@gmail.com>
2022-12-23 20:07:49 +0530

    feat:added search

--- controller/products.js
+++ controller/products.js
@@ -3,12 +3,13 @@ const products = require("../models/product");
 const getAllProducts = async (req, res) => {
   const { company, name, sort, select } = req.query;
   const queryObject = {};
-  let apiData = products.find(queryObject);
+
   if (company) {
-    queryObject.company = company;
+    queryObject.company = { $regex: company, $options: "i" };
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+    console.log(queryObject);
   }
   if (sort) {
     let sortFix = sort.replace(",", " ");
@@ -18,18 +19,24 @@ const getAllProducts = async (req, res) => {
     let selectFix = select.split(",").join(" ");
     apiData = apiData.select(selectFix);
   }
+  let apiData = products.find(queryObject);
   let page = Number(req.query.page) || 1;
   let limits = Number(req.query.limit) || 3;
 
   let skipPage = (page - 1) * limits;
   apiData = await apiData.skip(skipPage).limit(limits);
-
   const ProductDetails = await apiData;
+
   res.status(200).json({ ProductDetails, totalData: ProductDetails.length });
 };
 
 const getAllTesting = async (req, res) => {
-  const TestingDetails = await products.find(req.query).sort("name -price");
+  const { company, name, sort } = req.query;
+  const queryObject = {};
+  if (company) {
+    queryObject.company = { $regex: company, $options: "i" };
+  }
+  const TestingDetails = await products.find(queryObject);
   res.status(200).json({ TestingDetails });
 };
 
